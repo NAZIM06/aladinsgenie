f26c7f7a868898ad548b55ed6027c8c76fbf2e40
= <tamimmuktadir@gmail.com>
2021-06-12 22:25:58 +0600

    added wishlist toggle and price to card

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -4,7 +4,7 @@ import PdCardMd from "./../../productCardMd/PdCardMd";
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+      <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         <PdCardMd />
         <PdCardMd />
         <PdCardMd />
--- src/components/productCardMd/PdCardMd.js
+++ src/components/productCardMd/PdCardMd.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { CgHeart } from "react-icons/cg";
+import { FaHeart } from "react-icons/fa";
 import tshirt from "../../assets/black-t-shirt.jpg";
 import IconBtn from "../iconBtn/IconBtn";
 import Image from "../image/Image";
 
 export default function PdCardMd() {
+  const [wish, setWish] = useState(false);
   return (
     <div className="px-2 py-2 my-2 bg-white rounded">
       <div className="relative">
         <Image src={tshirt} alt="Product Image 16x9" width="10" height="10" />
-        <CgHeart className="absolute text-2xl text-gray-600 top-2 right-2" />
+        <div
+          className="absolute top-0 right-0 p-2 text-2xl text-gray-600 cursor-pointer"
+          onClick={() => setWish(!wish)}
+        >
+          {wish ? <FaHeart /> : <CgHeart />}
+        </div>
+        <h2 className="absolute bottom-0 left-0 p-2 text-2xl font-medium text-gray-500">
+          ৳ 450
+        </h2>
       </div>
       {/* Card footer */}
       <div className="flex items-center justify-between py-3 text-gray-500 text-md">
-        <h3 className="">Lorem ipsum dolor sit.</h3>
+        <div>
+          <h1 className="font-medium">Lorem ipsum dolor sit.</h1>
+        </div>
         <IconBtn />
       </div>
     </div>
