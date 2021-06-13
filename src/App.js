2d02129467810efb00573c610c402df1aa73fbe4
= <tamimmuktadir@gmail.com>
2021-06-13 15:05:09 +0600

    added redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
+import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HeaderNav from "./components/headerNav/HeaderNav";
 import Home from "./components/pages/home/Home";
 import SingleProduct from "./components/pages/singleProPg/SingleProduct";
+import store from "./redux/Store";
 function App() {
   return (
-    <div className="bg-gray-300">
-      <Router>
-        <HeaderNav />
-        <Switch>
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
-          <Route path="/single-product">
-            <SingleProduct></SingleProduct>
-          </Route>
-        </Switch>
-      </Router>
-    </div>
+    <Provider store={store}>
+      <div className="bg-gray-300">
+        <Router>
+          <HeaderNav />
+          <Switch>
+            <Route exact path="/">
+              <Home></Home>
+            </Route>
+            <Route path="/single-product">
+              <SingleProduct></SingleProduct>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </Provider>
   );
 }
 
